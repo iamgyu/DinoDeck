54fe4f2e190fb3e10b65c6a7c3e93e66170edfe0
iamgyu <iamgyu@naver.com>
2016-10-03 20:29:00 +0900

    Tilemap1

---
+++ T‎ilemap1/manifest.lua
@@ -0,0 +1,20 @@
+--
+-- A manifest of all the game's assets test
+--
+manifest =
+{
+    scripts =
+    {
+        ['main.lua'] =
+        {
+            path = "main.lua"
+        },
+    },
+    textures =
+    {
+        ['grass_tile.png'] =
+        {
+            path = "grass_tile.png",
+        },
+    }
+}
