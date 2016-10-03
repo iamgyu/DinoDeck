a9a02c7cc433394600e1984f2cbeeab4c327ba31
iamgyu <iamgyu@naver.com>
2016-10-03 22:15:08 +0900

    Tilemap9

--- Tilemap9/main.lua
+++ Tilemap9/main.lua
@@ -7,7 +7,7 @@ gRenderer = Renderer:Create()
 gTileSprite = Sprite:Create()
 gTileSprite:SetTexture(Texture.Find("uv_example.png"))
 
-gTileSprite:SetUVs(0, 0, 1, 1)
+gTileSprite:SetUVs(0.5, 0.5, 1, 1)
 --gTileSprite:SetUVs(0.25,0.25,0.75,0.75) -- draws half the image from the center
 function update()
     gRenderer:DrawSprite(gTileSprite)
